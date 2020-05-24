1e7dee2c29719237c91160dc09719f41af09b17b
Nick <nthechastener@mail.ru>
2020-05-24 22:51:05 +0600

    images changing done and images fixed

--- js/products.js
+++ js/products.js
@@ -4,5 +4,45 @@ $(function () {
       $('.header-top__currency-item').toggleClass('open');
       $('.header-top__currency-list').toggleClass('open');
    });
+
+   $('#bar-rating').barrating({
+      theme: 'bars-reversed',
+      hoverState: false,
+      showSelectedRating: false,
+      initialRating: 5
+   });
+
+   let imageBig = $('.product__images').find('img')[0];
+   // let imagesSmall = $('.product__images-small').find('img');
+   let imagesSmall = document.querySelectorAll('.product__image-small');
+   let imagesSmallParent = $('.product__images');
+
+   console.log(imageBig);
+   console.log(imagesSmall);
    
+
+   $(imagesSmallParent).on('click', function (event) {
+      let target = event.target;
+      let imagePath;
+
+      if (target && $(target).hasClass('product__image-small')) {
+
+         for (let i = 0; i < imagesSmall.length; i++) {
+
+            if (target == imagesSmall[i]) {
+               imagePath = '../img/products/sofi-blouse-' + (i + 1) + '.jpg';
+               console.log('vot' + imagesSmall[i]);
+
+            }
+         }
+         
+         changeImage(imagePath);
+      }
+   });
+
+
+   function changeImage(str) {
+      imageBig.setAttribute('src', str);
+   }
+
 });
