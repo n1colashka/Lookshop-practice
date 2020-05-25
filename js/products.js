e1f7051533ec7b69cc1d9c97506f1d1c784dbed7
Nick <nthechastener@mail.ru>
2020-05-26 00:24:45 +0600

    slide menu done

--- js/products.js
+++ js/products.js
@@ -5,12 +5,14 @@ $(function () {
       $('.header-top__currency-list').toggleClass('open');
    });
 
+   // clothes slider
    $('.clothes-slider__content').slick({
       arrows: true,
       dots: true,
       draggable: false
    });
 
+   // range slider
    $("#slider-range").slider({
       range: true,
       min: 0,
@@ -22,5 +24,30 @@ $(function () {
    });
    $("#filter-range").val("$" + $("#slider-range").slider("values", 0) +
       " - $" + $("#slider-range").slider("values", 1));
+
+
+   let categoriesList = $('.filter__categories-list');
+   let categoriesItems = $('.filter__categories-item-title');
+
+   $(categoriesList).on('click', function (event) {
+      let target = event.target;
+
+      if (target && $(target).hasClass('filter__categories-item-title')) {
+         for (let i = 0; i < categoriesItems.length; i++) {
+            if (target == categoriesItems[i]) {
+               $(categoriesItems[i]).next().toggle('open'); 
+               $(categoriesItems[i]).parent().toggleClass('open'); 
+            }
+         }
+      }
+   });
+
+   let filterBtns = $('.filter__sizes-btn');
+
+   // $(filterBtns).on('click', function(event) {
+   //    $(this).toggleClass('active');
+   // });
+
+
 
 });
