c9cd49ba8b108d41f2a64c31fbd685ba9fb7a19f
Nick <nthechastener@mail.ru>
2020-05-26 11:54:37 +0600

    clothes active btns and subitems done

--- js/products.js
+++ js/products.js
@@ -25,7 +25,7 @@ $(function () {
    $("#filter-range").val("$" + $("#slider-range").slider("values", 0) +
       " - $" + $("#slider-range").slider("values", 1));
 
-
+   // filter drop down list
    let categoriesList = $('.filter__categories-list');
    let categoriesItems = $('.filter__categories-item-title');
 
@@ -35,18 +35,56 @@ $(function () {
       if (target && $(target).hasClass('filter__categories-item-title')) {
          for (let i = 0; i < categoriesItems.length; i++) {
             if (target == categoriesItems[i]) {
-               $(categoriesItems[i]).next().toggle('open'); 
-               $(categoriesItems[i]).parent().toggleClass('open'); 
+               $(categoriesItems[i]).next().toggle('open');
+               $(categoriesItems[i]).parent().toggleClass('open');
             }
          }
       }
    });
 
+   // subitems click
+   let subItems = $('.filter__categories-subitem-title');
+
+   $(subItems).on('click', function (event) {
+      let target = event.target;
+
+      for (let i = 0; i < subItems.length; i++) {
+         if (target == subItems[i]) {
+
+            for (let j = 0; j < subItems.length; j++) {
+               $(subItems[j]).parent().removeClass('current');
+            }
+            $(subItems[i]).parent().addClass('current');
+         }
+      }
+   });
+
+   // size btns click
    let filterBtns = $('.filter__sizes-btn');
 
-   // $(filterBtns).on('click', function(event) {
-   //    $(this).toggleClass('active');
-   // });
+   $(filterBtns).on('click', function (event) {
+      let target = event.target;
+
+      for (let i = 0; i < filterBtns.length; i++) {
+         if (target == filterBtns[i]) {
+
+            for (let j = 0; j < filterBtns.length; j++) {
+               $(filterBtns[j]).removeClass('active');
+            }
+            $(filterBtns[i]).addClass('active');
+         }
+      }
+   });
+
+   // filter btn click
+
+   let filterRangeBtn = $('.filter__range-btn');
+
+   $(filterRangeBtn).on('click', function(){
+      $(this).addClass('active');
+   });
+
+
 
 
 
