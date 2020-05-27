f4263bbfd46b990b5794de6daa1fef7e1a77be1a
Nick <nthechastener@mail.ru>
2020-05-28 01:59:58 +0600

    products, clearance adaptive done

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ $(function () {
         arrows: true,
         dots: true,
         draggable: false,
-
     });
 
     $('.products__slider').slick({
@@ -25,13 +24,13 @@ $(function () {
                 }
             },
             {
-                breakpoint: 1400,
+                breakpoint: 1600,
                 settings: {
                     slidesToShow: 5
                 }
             },
             {
-                breakpoint: 1300,
+                breakpoint: 1330,
                 settings: {
                     slidesToShow: 4,
                     centerMode: false,
@@ -47,14 +46,34 @@ $(function () {
                 }
             },
             {
-                breakpoint: 680,
+                breakpoint: 650,
                 settings: {
                     slidesToShow: 2,
                     centerMode: false,
+                    draggable: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 455,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                    draggable: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: false,
+                    draggable: true,
+                    arrows: false
                 }
             }
         ]
-        // adaptiveHeight: true 
+
     });
 
     $(".owl-carousel").owlCarousel({
@@ -94,6 +113,10 @@ $(function () {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
+            if (screen.width <= '650') {
+                tab[i].classList.remove('show');
+                tab[i].classList.add('hide');
+            }
         }
     }
 
@@ -103,8 +126,29 @@ $(function () {
         if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
+            if (screen.width <= '650') {
+                tab[b].classList.remove('hide');
+                tab[b].classList.add('show');
+            }
         }
     }
+
+    $(info).on('click', function (event) {
+
+        if (screen.width <= '650') {
+            let target = event.target;
+
+            for (let i = 0; i < tab.length; i++) {
+                if (tab[i] != target) {
+                    $(tab[i]).slideToggle(500);
+                }
+                $(tab[i]).toggleClass('list-delete');
+            }
+        }
+
+    });
+
+
 
     info.addEventListener('click', function (event) {
         let target = event.target;
@@ -112,19 +156,20 @@ $(function () {
         if (target && target.classList.contains('products__tab')) {
             for (let i = 0; i < tab.length; i++) {
 
-                if (target == tab[i]) {
+                if (target == tab[i] && !($(tab[i]).hasClass('current'))) {
                     hideTabContent(0);
                     showTabContent(i);
                     for (let i = 0; i < tab.length; i++) {
                         tab[i].classList.remove('current');
                     }
                     tab[i].classList.add('current');
+                    $(".products__slider").slick('slickSetOption', 'adaptiveHeight', true, true);
                     break;
                 }
 
             }
-            // $(".products__slider").slick('reinit');
-            $(".products__slider").slick('slickSetOption', 'adaptiveHeight', true, true);
+
+
         }
     });
 
@@ -176,4 +221,6 @@ $(function () {
             event.preventDefault();
         }
     });
+
+
 });
